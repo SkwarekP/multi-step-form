63909fa20fc521f67654e9d6f84c51a63292369f
Patryk <patryk.skwara750@gmail.com>
2023-01-30 16:31:16 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "../src/assets/style.scss";
 import {Routes, Route, useNavigate} from "react-router-dom";
 
 function App() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     // useEffect(() => {
     //     navigate("/");
