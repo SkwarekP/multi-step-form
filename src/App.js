f18463c529bb9d362cf962ad724e5d8c6e94f7f6
Patryk <patryk.skwara750@gmail.com>
2023-01-30 17:02:08 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import PickAdd from "./StepsPages/PickAdd";
 import Summary from "./StepsPages/Summary";
 import FinishInfo from "./StepsPages/FinishInfo";
 import "../src/assets/style.scss";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 function App() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate("/");
+        //eslint-disable-next-line
+    }, [])
 
     return (
         <Routes>
